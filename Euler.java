501ecaf374e35e43b528416780d6a40216c4eb97
Viren Sabhnani <vmsabhnani@gmail.com>
2016-12-10 20:12:07 -0500

    Added Solution to #2

--- Euler.java
+++ Euler.java
@@ -3,6 +3,7 @@ public class Euler {
 
     public static void main(String[] args) {
         problem1();
+        problem2();
     }
     
     public static void problem1(){ // Find the sum of all the multiples of 3 or 5 below 1000.
@@ -14,6 +15,18 @@ public class Euler {
         }
         System.out.println(sum);
     }
-
-    
+    public static void problem2(){ //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms. 
+        int sum = 0;
+	int f1 = 1;  
+	int f2 = 2; 
+        int next;
+        while (f1 <= 4000000) {
+            if (f1 % 2 == 0)
+		sum += f1;
+            next = f1 + f2;
+            f1 = f2;
+            f2 = next;
+	}
+        System.out.println(sum);  
+    }
 }
